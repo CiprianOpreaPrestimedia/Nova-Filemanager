9d5173826457fac627150a2c18ba2bcd73e2fa82
Ciprian OPREA <39329521+CiprianOpreaPrestimedia@users.noreply.github.com>
2020-06-15 16:57:14 +0300

    Update DefaultNamingStrategy.php

--- src/Http/Services/DefaultNamingStrategy.php
+++ src/Http/Services/DefaultNamingStrategy.php
@@ -16,14 +16,37 @@ class DefaultNamingStrategy extends AbstractNamingStrategy
     {
         $filename = $file->getClientOriginalName();
 
-        while ($this->storage->has($currentFolder.'/'.$filename)) {
+        <?php
+
+namespace Infinety\Filemanager\Http\Services;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+
+class DefaultNamingStrategy extends AbstractNamingStrategy
+{
+    /**
+     * @param string $currentFolder
+     * @param UploadedFile $file
+     * @return mixed
+     */
+    public function name(string $currentFolder, UploadedFile $file): string
+    {
+        $filename = $file->getClientOriginalName();
+
+        /*while ($this->storage->has($currentFolder.'/'.$filename)) {
             $filename = sprintf(
                 '%s_%s.%s',
                 pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME),
                 Str::random(7),
                 $file->getClientOriginalExtension()
             );
-        }
+        }*/
+
+        return $filename;
+    }
+}
+
 
         return $filename;
     }
